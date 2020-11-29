5cbab3c012644adf28676327f75bf26fe400c4b1
Charith Kulawardana <cbkulawardana@gmail.com>
2020-11-29 21:16:04 +0800

    Rename dart files

--- lib/ui/GradientExample.dart
+++ lib/ui/GradientExample.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'gradient_home.dart';
+import 'child/gradient_home.dart';
 
 void main() => runApp(MyApp());
 
