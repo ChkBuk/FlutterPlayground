10694d79343d75537aba23593f0028d50d18a2b2
Charith Kulawardana <cbkulawardana@gmail.com>
2020-11-29 11:21:41 +0800

    Added fonts directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  theme: ThemeData(fontFamily: 'IndieFlower'),
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Hey Charith..!'),
-      centerTitle: true,
-      backgroundColor: Colors.pink,
-    ),
-    body: Center(
-      child: Text(
-        'Jenuli Shenara',
-        style: TextStyle(
-            color: Colors.black87,
-            fontSize: 30.0,
-            fontFamily: 'IndieFlower'
+      theme: ThemeData(fontFamily: 'IndieFlower'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hey Charith..!'),
+          centerTitle: true,
+          backgroundColor: Colors.pink,
+        ),
+        body: Center(
+          child: Text(
+            'Dodampe Gamage Jenuli Shenara',
+            style: TextStyle(
+                color: Colors.black87,
+                fontSize: 25.0,
+                fontFamily: 'IndieFlower'),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          label: Text('Approve'),
+          backgroundColor: Colors.pink,
         ),
       ),
-
-    ),
-    floatingActionButton: FloatingActionButton.extended(
-      onPressed: () {
-        // Add your onPressed code here!
-      },
-      label: Text('Approve'),
-      backgroundColor: Colors.pink,
-    ),
-  ),
-));
+    ));
